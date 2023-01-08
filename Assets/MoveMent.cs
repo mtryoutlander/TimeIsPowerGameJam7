4142948512e2ea8d45bf40c89eb4fe244d2b778b
Markus <markus@hitanykey.biz>
2023-01-08 15:38:12 -0600

    add player movement and cammra fallow + add grid and tile map

--- Assets/MoveMent.cs
+++ Assets/MoveMent.cs
@@ -7,11 +7,12 @@ using System;
 public class MoveMent : MonoBehaviour
 {
     Rigidbody2D body;
+    private bool isMoving = false;
 
     float horizontal;
     float vertical;
 
-    public float runSpeed = 10.0f;
+    public float runSpeedX, runSpeedY;
     
     
     // Start is called before the first frame update
@@ -23,13 +24,33 @@ public class MoveMent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
+        if(!isMoving && (Input.GetKeyDown(KeyCode.W)||Input.GetKeyDown(KeyCode.UpArrow))){
+            isMoving = true;
+            vertical = Input.GetAxisRaw("Vertical");
+        }
+        if(!isMoving && (Input.GetKeyDown(KeyCode.S)||Input.GetKeyDown(KeyCode.DownArrow))){
+            isMoving = true;
+            vertical = Input.GetAxisRaw("Vertical");
+        }
+        if(!isMoving && (Input.GetKeyDown(KeyCode.A)||Input.GetKeyDown(KeyCode.LeftArrow))){
+            isMoving = true;
+            horizontal = Input.GetAxisRaw("Horizontal");
+
+        }
+        if(!isMoving && (Input.GetKeyDown(KeyCode.D)||Input.GetKeyDown(KeyCode.RightArrow))){
+            isMoving = true;
+            horizontal = Input.GetAxisRaw("Horizontal");
+        }
+
+        
     }
 
     private void FixedUpdate()
     {
-        body.velocity = new UnityEngine.Vector2(horizontal * runSpeed, vertical * runSpeed);   
+        body.transform.position += new UnityEngine.Vector3(horizontal * runSpeedX, vertical * runSpeedY, 0); 
+        horizontal =0;
+        vertical =0;
+        isMoving =false;
     }
    
 }
