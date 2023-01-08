df39f419dd009b05312672ec2de400ce6d078f1b
Markus <markus@hitanykey.biz>
2023-01-07 23:00:40 -0600

    install

---
+++ Assets/MoveMent.cs
@@ -0,0 +1,35 @@
+using System.Numerics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MoveMent : MonoBehaviour
+{
+    Rigidbody2D body;
+
+    float horizontal;
+    float vertical;
+
+    public float runSpeed = 10.0f;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        body.velocity = new UnityEngine.Vector2(horizontal * runSpeed, vertical * runSpeed);   
+    }
+   
+}
